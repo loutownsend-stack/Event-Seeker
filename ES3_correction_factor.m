3d3eebb07964ad6c65bccb8fbbc00e16b092a91f
loutownsend-stack <lou.townsend2022@gmail.com>
2026-04-27 09:37:59 -0500

    Update ES3_correction_factor.m
    Added ratioing sequence after "else" in nested "if" to ensure it runs when user inputs "0" to continue.

--- ES3_correction_factor.m
+++ ES3_correction_factor.m
@@ -37,7 +37,109 @@ end
          ES3_correction_factor
 
     else
-           
+        ArrayG1(:,1) = ArrayG(:,1);
+            ArrayP1(:,1) = ArrayP(:,1);
+
+
+            %Events captured based on Z-score of the ratio of CH1 signal to
+            %CH2 signal. 
+            ratio = ArrayG1./ArrayP1;
+            catcher = zeros(length(ratio), (width(ratio))); %preallocating loop data 
+            catcher(:,1)= ArrayG(:,1); %time column is fixed
+            
+            %the following loop takes a z-score for every point in the ratio array,
+            %using per sweep column average and std dev in the z-calculation. 
+            %this loop fills an empty "catcher array" with a 1 only if the z-score 
+            %conditions depicted in the if/else loop are satisfied (ie. ~2 std 
+            %deviations above the mean = an event (1) )
+            
+            for vv = 2:width(ratio)  %note we iterate columnwise starting at column 2, column 1 is time 
+                for bb = 1:length(ratio) %rowwise iteration
+                    if (ratio(bb,vv)-mean(ratio(:,vv)))/std(ratio(:,vv))> Z %z-score   
+                        catcher(bb,vv) = 1;
+                       
+                    else
+                        
+                    end
+                end
+            end
+            display("Results by Ratio Z-score Method baseline subbed")
+            events_per_sweep(1,:)= sum(catcher(:,2:width(catcher))) %sums events/sweep 
+            find_events = find(catcher == 1);
+            num_events = length(find_events)
+            highlight_events = catcher.*ratio;
+            mean_events_per_second = mean(events_per_sweep)/(catcher(length(catcher),1)/1000)
+            if mean_events_per_second == 0
+                display("rerun with higher correction factor")
+            else
+            end
+            
+            only_events = catcher(:,2:width(catcher));
+            
+            %This section organizes the data into a one X and Y column only, rather
+            %than X Y Y Y Y it becomes
+            % X Y
+            % X Y
+            % X Y   for ease of binning across all sweeps
+            format_build = zeros((length(catcher)*(width(catcher)-1)),2); %sizing
+            format_build(1:length(catcher),1) = catcher(:,1); %first time col
+            format_build(1:length(catcher),2) = only_events(:,1); %binary events col
+            
+            for gg = 1: width(only_events)
+                mm = gg+1;
+                if mm <= width(only_events)
+                    format_build((((gg)*length(only_events)+1):((mm)*length(only_events))),2)= only_events(:,mm);
+                    format_build((((gg)*length(only_events)+1):((mm)*length(only_events))),1)= catcher(:,1);
+                else
+                   
+                end
+            end
+            
+                    
+            positional_time = find(format_build(:,2) == 1); %Finds event position
+            
+            %Loop calculates inter event intervals, taking the time of one event minus
+            %the time of the nearest previous event to generate the interval between
+            
+                %Note conditional loop mandates that timing at positional_time(o) must be greater than for time at (n)
+                %to avoid false interval calculation
+            for n = 1:length(positional_time)
+                o = n+1;
+                if o <= length(positional_time) 
+                    if format_build(positional_time(o)) > format_build(positional_time(n))
+                    inter_event_interval(n) = format_build(positional_time(o))-format_build(positional_time(n));
+                    else
+                    end
+                else
+                end
+            end
+            
+            
+            ms = inter_event_interval';  
+            interevents_table = array2table(ms) ; %table of event durations in order
+            
+            %Binning operation and histogram generation
+            binwidth = input('Enter the binwidth desired here: ', 's');
+            binwidth_numeric = str2num(binwidth);
+            figure
+            title('Fitted Intervals Cell 2')
+            number_of_bins = round((max(ms)/binwidth_numeric));
+            %histogram(inter_event_interval,"BinWidth",10)
+            histfit(inter_event_interval,number_of_bins,"exponential")
+                
+            xlabel('Interval Duration (ms)')
+            ylabel('Number of Inter Event Intervals')
+            
+            %% For individual sweep Raster-like plotting of events
+            
+            raster_table = zeros(length(catcher),width(catcher));
+            raster_table(:,1) = catcher(:,1);
+            for t = 2:width(catcher)
+                    raster_table(:,t) = catcher(:,t)*(t-1); %multiplies catcher by sweep #
+            end
+            
+            %plotting then makes 1's for events detected in sweep one, 2's for sweep 2, 3 for sweep 3, and so on for easy
+            %visualization         
     end     
  else 
     ArrayG1(:,1) = ArrayG(:,1);
